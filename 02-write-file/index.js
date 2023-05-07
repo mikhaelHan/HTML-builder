c5fd2d6d3dbbbe715bea3ef1bbd88e9a51254722
mikhaelHan <hancharuk270185@mail.ru>
2023-05-08 02:30:51 +0300

    refactor: refactor code in the subtask 01-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,28 +1,34 @@
+const { error } = require('console');
 const fs = require('fs');
 const path = require('path');
 const { stdin, stdout, } = process;
-const file = path.dirname(__filename);
 
+fs.writeFile(
+  path.join(__dirname, 'text.txt'),
+  '',
+  (err) => {
+    if (err) throw error;
 
-fs.writeFile(`${file}/text.txt`, '', (err) => {
-  if (err) console.log(err);
-  else {
-    stdout.write('Please enter text...\n');
+    stdout.write('Please enter text... \n');
+
     stdin.on('data', data => {
       const info = data.toString().trim();
+
       if (info !== 'exit') {
-        fs.appendFile(`${file}/text.txt`, `\n${info}`, (err) => {
-          if (err) console.log(err);
-        });
-      }
-      else {
+        fs.appendFile(
+          path.join(__dirname, 'text.txt'),
+          info + '\n',
+          err => { if (err) throw error; }
+        );
+      } else {
         stdout.write('Good bye!!!');
         process.exit();
       }
     });
+
     process.on(('SIGINT'), () => {
       stdout.write('Good bye!!!');
       process.exit();
     });
   }
-});
+);
