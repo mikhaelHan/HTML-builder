34849565786fab44ef38d9523ac8e1be2fb6bd2f
mikhaelHan <hancharuk270185@mail.ru>
2023-05-08 02:28:53 +0300

    refactor: refactor code in the subtask 01-read-files

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const file = path.dirname(__filename);
-const stream = fs.createReadStream(`${file}/text.txt`, 'utf-8');
+
+const file = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(file, 'utf-8');
+
 let data = '';
+
 stream.on('data', chunk => data += chunk);
 stream.on('end', () => console.log(data));
 stream.on('error', error => console.log('Error', error.message));
