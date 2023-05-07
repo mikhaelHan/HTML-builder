7466bfe5a4ebdd5c87afb54c836caed0984de559
mikhaelHan <hancharuk270185@mail.ru>
2023-05-08 02:32:16 +0300

    refactor: refactor code in the subtask 01-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,39 @@
-const { error } = require('console');
-const fs = require('fs/promises');
-const path = require('path');
-const direct = path.join(__dirname, 'secret-folder');
+class FilesInfo {
+  constructor () {
 
-async function readding() {
-  const resp = await fs.readdir(direct, { withFileTypes: true });
-  resp.forEach(async (file) => {
-    if (file.isFile()) {
-      const fileBox = (file.name).split('.');
-      const data = await fs.stat(path.join(direct, file.name));
-      let vol = Math.round((data.size / 1024) * 100) / 100;
-      console.log(`${fileBox[0]} - ${fileBox[1]} - ${vol}kb`);
+    this.fs = require('fs/promises');
+    this.error = require('console');
+    this.path = require('path');
+    this.direct = this.path.join(__dirname, 'secret-folder');
+
+  }
+
+  async displayFiles() {
+    const response = await this.fs.readdir(this.direct, { withFileTypes: true });
+
+    for (let file of response) {
+      if (file.isFile()) {
+
+        const fileBox = (file.name).split('.');
+
+        const data = await this.fs.stat(this.path.join(this.direct, file.name));
+
+        const volume = Math.round((data.size / 1024) * 1000) / 1000;
+
+        console.log(`${fileBox[0]} - ${fileBox[1]} - ${volume}kb`);
+      }
     }
-  });
+  }
 }
+
+
+
 try {
-  readding();
+
+  const filesInfo = new FilesInfo();
+  filesInfo.displayFiles();
+
 } catch (err) {
-  throw error;
+
+  throw this.error;
 }
