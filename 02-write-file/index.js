e9a5f61c2f4033d6e60dbcbdd29c89d607c16e6d
mikhaelHan <hancharuk270185@mail.ru>
2023-05-08 04:21:44 +0300

    refactor: refactor code in the subtask 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,34 +1,55 @@
-const { error } = require('console');
-const fs = require('fs');
-const path = require('path');
 const { stdin, stdout, } = process;
 
-fs.writeFile(
-  path.join(__dirname, 'text.txt'),
-  '',
-  (err) => {
-    if (err) throw error;
+class ConsoleWrite {
+  constructor () {
 
-    stdout.write('Please enter text... \n');
+    this.error = require('console');
+    this.fs = require('fs');
+    this.path = require('path');
 
-    stdin.on('data', data => {
-      const info = data.toString().trim();
-
-      if (info !== 'exit') {
-        fs.appendFile(
-          path.join(__dirname, 'text.txt'),
-          info + '\n',
-          err => { if (err) throw error; }
-        );
-      } else {
-        stdout.write('Good bye!!!');
-        process.exit();
-      }
-    });
-
-    process.on(('SIGINT'), () => {
-      stdout.write('Good bye!!!');
-      process.exit();
-    });
   }
-);
+
+
+  writeInfo() {
+    this.fs.writeFile(
+      this.path.join(__dirname, 'text.txt'),
+      '',
+      (err) => {
+        if (err) throw this.error;
+
+        stdout.write('Please enter text... \n');
+
+        stdin.on('data', data => {
+          const info = data.toString().trim();
+
+          if (info !== 'exit') {
+            this.fs.appendFile(
+              this.path.join(__dirname, 'text.txt'),
+              info + '\n',
+              err => { if (err) throw this.error; }
+            );
+          } else {
+            stdout.write('Good bye!!!');
+            process.exit();
+          }
+        });
+
+        process.on(('SIGINT'), () => {
+          stdout.write('Good bye!!!');
+          process.exit();
+        });
+      }
+    );
+  }
+}
+
+
+try {
+
+  const consoleWrite = new ConsoleWrite();
+  consoleWrite.writeInfo();
+
+} catch (err) {
+
+  throw this.error;
+}
